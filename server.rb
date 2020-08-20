bf673adef7fa46a965113caf6a24a8daba1f6430
adr29truck <adrianalmetunsmeds@gmail.com>
2020-08-20 15:05:23 +0200

    Remove excessive logging

--- server.rb
+++ server.rb
@@ -8,7 +8,6 @@ class Server < Sinatra::Base
     get "/game" do
         @people = File.read "people.json"
         @people = JSON.parse(@people)
-        p @people.class  
         slim :game 
     end 
 end
