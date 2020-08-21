633c96df7d921b1a450e872896265ed4d67c08b7
adr29truck <adrianalmetunsmeds@gmail.com>
2020-08-21 16:00:07 +0200

    Added game ending

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    if (location.href.includes('/game')) {
     game()  
+    } else {
+        window.localStorage.clear();
+    }
 })
 
 function game() {
@@ -10,15 +14,41 @@ function game() {
     console.log(data)
     
     wrapper = document.querySelector('body')
-    console.log(data[0])
-    console.log(data)
     dataLength = Object.keys(data).length
-    console.log(dataLength)
     running = true
     while (running) {
-        keys = Object.keys(window.localStorage)
-        if (window.localStorage.getItem())
-        slump = Math.random(dataLength - 1)*dataLength -1
+        counter = window.localStorage.getItem('answerCounter');
+
+        // You have gussed on all images, and now the game is over
+        if (parseInt(counter) == dataLength * 4) {
+            popup = document.createElement('div')
+            title = document.createElement('h3')
+            title.innerHTML = 'Du klararde det.'
+            popup.append(title)
+
+            resetButton = document.createElement('p')
+            resetButton.innerHTML = 'Restart'
+            resetButton.classList.add("reset")
+            resetButton.onclick = function () {
+                resetGame()
+            }
+            popup.append(resetButton)
+            learnMore = document.createElement('a')
+            learnMore.innerHTML = 'Se alla bilder'
+            learnMore.href = "/"
+            popup.append(learnMore)
+
+            wrapper.append(popup)
+
+            running = false
+            return ''
+        }
+
+        slump = Math.random(dataLength - 1) * dataLength - 1
+        if (slump < 0) {
+            slump = slump * -1
+        }
+
         randomName = data[Math.round(slump)]['name']
   
         temp = window.localStorage.getItem(randomName);
@@ -53,6 +83,14 @@ function guess(el) {
         wrapper.style.background = 'lightgreen'
         // Fetches the current amount of correct guesses on a item.
         temp = window.localStorage.getItem(svar);
+        counter = window.localStorage.getItem('answerCounter');
+        if (counter != null && counter != 'undefined') {
+            counter = parseInt(counter) + 1
+        } else {
+            counter = 1
+        }
+        window.localStorage.setItem('answerCounter', counter);
+
         // Updates the number of correct guesses.
         if (temp != null && temp != 'undefined') {
             temp = parseInt(temp) + 1
@@ -63,9 +101,21 @@ function guess(el) {
 
         setTimeout(() => {
             location.reload();
-        }, 2500);
+        }, 1000);
+    } else {
+        temp = window.localStorage.getItem(svar);
+
+        counter = window.localStorage.getItem('answerCounter');
+
+        if (counter != null && counter != 'undefined') {
+            if (temp != null && temp != 'undefined') {
+                counter = parseInt(counter) - temp
+            }
+        } else {
+            counter = 0
     } 
-    else {
+        window.localStorage.setItem('answerCounter', counter);
+
         window.localStorage.setItem(svar, 0);
 
         wrapper.style.background = 'red'
@@ -75,6 +125,7 @@ function guess(el) {
     }
 }
 
-function reset() {
+function resetGame() {
     window.localStorage.clear();
+    location.reload()
 }
